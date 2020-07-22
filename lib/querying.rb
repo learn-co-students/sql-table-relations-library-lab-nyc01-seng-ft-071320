fc9d1ff48c374dd8234cd136a0ac164ab99c18e2
amitdesh <amitdesh2014@gmail.com>
2020-07-22 15:36:37 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id WHERE series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY length(characters.motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY characters.species ORDER BY characters.species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series, subgenres ON series.author_id = authors.id AND series.subgenre_id == subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM characters INNER JOIN series ON characters.author_id = series.author_id  WHERE characters.species = 'human' ORDER BY characters.species DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) AS frequency FROM character_books INNER JOIN characters ON character_books.character_id = characters.id GROUP BY character_books.character_id ORDER BY frequency DESC"
 end
